2d3d057f4dc89f34d3a8acd79d91c77466dee050
SirJhyx <114629645+SirJhyx@users.noreply.github.com>
2022-11-11 18:23:08 +0800

    JS Operators

---
+++ Activity/script.js
@@ -0,0 +1,34 @@
+console.log("connected");
+// 1.What is the difference between a comparison operator and an assignment operator?
+
+// 2.What is a Boolean? What does it represent?
+
+// 3.How is the += operator different than + operator?
+
+ //Answer:
+//1.  the difference between the two operator is the sign that they use which is represent itslelf, like when we use a comparison operator we use (==) and assignment operator we ise (=) there for = operator is used to assign variable and == operator use to compare two variable
+
+//2. boolean refer as TRUE or FALSE and it represent as TRUE or FALSE
+
+//3. i think the + sign itself use to add a numbers and += sign use to add and compare the two values i guess
+
+// Code Part
+
+// 1.Set two variables equal to two different numbers. Use a comparison operator to compare these two variables. Change one of their values by using the +=, -=, *=, or /= operator. Then, compare their values again.
+
+// 2.Try out the <= and >= operators.
+
+// 3.Try to use the !== operator.
+
+let x = 20;
+let y = 30;
+
+console.log(x += y);
+console.log(x -= y);
+console.log(x *= y);
+console.log(x /= y);
+
+console.log(x<=y);
+console.log(x>=y);
+
+console.log(y !== x);
